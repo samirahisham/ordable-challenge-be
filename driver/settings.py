e666cd448983afad550492e827c3ab75c73857bd
Samira Awad <engineersamiraawad@gmail.com>
2022-03-02 18:49:38 +0300

    update database

--- driver/settings.py
+++ driver/settings.py
@@ -123,8 +123,8 @@ WSGI_APPLICATION = 'driver.wsgi.application'
 
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'driver',
+    'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
